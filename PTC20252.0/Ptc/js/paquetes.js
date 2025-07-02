13329f66a675b626402d996c6a03b9756db2056e
Paul-PTC <20210103@ricaldone.edu.sv>
2025-07-02 16:14:02 -0600

    Agregué el apartado de ganancia  pero solo como se ve sin el js

--- PTC20252.0/Ptc/js/paquetes.js
+++
@@ -1,9 +0,0 @@
-// Mostrar el modal cuando se hace clic en el botón flotante
-document.getElementById("btnAgregarLugar").addEventListener("click", () => {
-  document.getElementById("modalLugar").style.display = "flex";
-});
-
-// Ocultar el modal cuando se hace clic en "Cancelar"
-document.getElementById("btnCerrarModal").addEventListener("click", () => {
-  document.getElementById("modalLugar").style.display = "none";
-});
